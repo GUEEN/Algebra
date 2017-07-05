1e5db3018ce57202560b5a226022621f8ce7b3a3
Ivan Livinsky <livinskyivan@gmail.com>
2017-07-05 21:26:18 +0300

    Update Group.h

--- Group.h
+++ Group.h
@@ -6,25 +6,38 @@
 class Group
 {
 friend class SearchNode;
+friend Group D(int m);
 public:	
 	Group(int m);
 	Group(const Group& G);
 	Group(Group&& G);
 	Group& operator = (const Group& G);
 	Group& operator = (Group&& G);
+	Group  operator ^ (const Perm& P) const;
+	Group  operator * (const Group& G) const;
 	~Group();
-
+	
 	bool contains(const Perm& P) const;
 	void addGen(const Perm& P);
 
-	long long order() const;	
+	long long order() const;
+
+	bool isAbelian() const;
+	bool isEven() const;
+
+	bool operator <= (const Group& G) const;
+	bool operator >= (const Group& G) const;
+	bool operator == (const Group& G) const;
+	bool operator <  (const Group& G) const;
+	bool operator >  (const Group& G) const;
+	bool operator << (const Group& G) const;
+	bool operator >> (const Group& G) const;
 
 protected:
 	int n; // order of the permutation presentation
 	int u; // a fixed element
-
-	//vector<Perm*> Generators; // generators of the group
-	PermList Generators;
+	
+	PermList Generators; // generators of the group
 
 	Perm Orbit; // orbit of u
 	int NPoints; // number of points in Orbit	
@@ -41,7 +54,16 @@ Long Order(const Group& G)
 }
 */
 Group S(int m);
+Group A(int m);
 Group Z(int m);
+Group D(int m);
+Group K4();
+Group Q8();
+Group M11();
+Group M12();
+Group M22();
+Group M23();
+Group M24();
 
 #endif
 
